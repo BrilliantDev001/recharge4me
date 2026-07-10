56968b7e1562b2b60408373256107f40ef10c3c3
brilliantDev001 <marvelousakinola001@gmail.com>
2026-07-10 11:02:43 +0100

    Add SPA rewrite config for Vercel

--- server/src/app.js
+++ server/src/app.js
@@ -24,7 +24,10 @@ const settingsRoutes = require("./routes/settings.routes");
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: process.env.CLIENT_URL || "http://localhost:5173",
+  credentials: true,
+}));
 app.use(express.json());
 
 app.get("/", (req, res) => {
